72c6916883e4ef4299c9e6a6d57fd9b9b1bd0daa
Hanna Glattfelder <glati98@gmail.com>
2018-07-18 15:05:28 +0200

    kinetic_energy commit

--- debug_example/kinetic_energy.py
+++ debug_example/kinetic_energy.py
@@ -1,10 +1,11 @@
-# Sample Python/Pygame Programs
-# Simpson College Computer Science
-# http://programarcadegames.com/
-# http://simpson.edu/computer-science/
- 
-# Calculate Kinetic Energy
- 
+"""
+Sample Python/Pygame Programs
+Simpson College Computer Science
+http://programarcadegames.com/
+http://simpson.edu/computer-science/
+Calculate Kinetic Energy
+"""
+
 print("This program calculates the kinetic energy of a moving object.")
 m_string = input("Enter the object's mass in kilograms: ")
 m = float(m_string)
